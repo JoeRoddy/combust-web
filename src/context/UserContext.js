34fd41795371372db24c573e1f6e818c1e22d08c
Joe <mrjoeroddy@gmail.com>
2019-11-02 18:39:49 -0400

    clean up user context

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -18,20 +18,6 @@ function UserProvider({ children }) {
     server: serverInfo
   };
 
-  // /**
-  //  * callback executed when the user logs in, args: user
-  //  * @param {function} callback
-  //  */
-  // const onLogin = callback => _onLoginTriggers.push(callback);
-
-  // /**
-  //  * callback executed when the user logs out, args: user
-  //  * @param {function} callback
-  //  */
-  // const onLogout = callback => {
-  //   _onLogoutTriggers.push(callback);
-  // };
-
   /**
    * returns the public user info from a given user id
    * & applies a listener to their data
@@ -78,10 +64,9 @@ function UserProvider({ children }) {
         message: "You must provide an email and password"
       });
     }
-
     userDb.createUserWithEmail(user, (err, userDataByPrivacy) => {
       if (err) return callback(err);
-      _saveCurrentUserLocally(userDataByPrivacy);
+      saveCurrentUserLocally(userDataByPrivacy);
       callback(err, userDataByPrivacy);
     });
   };
@@ -93,7 +78,6 @@ function UserProvider({ children }) {
    */
   const searchByField = async (query, field) => {
     let results = await userDb.searchByField(query, field);
-    debugger;
     results = results || {};
     let formattedResults = [];
     Object.keys(results).forEach(uid => {
@@ -110,50 +94,7 @@ function UserProvider({ children }) {
    * sends a reset email to an email address if the account exists
    * @returns {Promise} password reset result
    */
-  const sendPasswordResetEmail = email => {
-    return userDb.sendPasswordResetEmail(email);
-  };
-
-  // Private members. Not accessible from views.
-
-  let _onLoginTriggers = [];
-  let _onLogoutTriggers = [];
-
-  const _listenToCurrentUser = function() {
-    console.log("applying listener");
-
-    userDb.listenToCurrentUser((err, userData) => {
-      console.log("listening to curr user:", userData);
-
-      if (err) {
-        return console.log(err);
-      } else if (!userData && userId) {
-        //user logged out
-        _handleUserLogout();
-        setUserId(null);
-      } else if (userData) {
-        //new data
-        let shouldExecEstablished = !user && userData.publicInfo;
-        _saveCurrentUserLocally(userData);
-        if (shouldExecEstablished) {
-          _handleUserEstablished({
-            id: userData.publicInfo.id,
-            publicInfo: userData.publicInfo
-          });
-        }
-      }
-    });
-  };
-
-  const _updateUser = function() {
-    const user = this;
-    const uid = user.id;
-    delete user.save;
-    delete user.id;
-    delete user.displayName;
-    userDb.saveToUsersCollection(uid, { publicInfo: user });
-    _savePublicUserInfo(uid, user); // reapply deleted properties
-  };
+  const sendPasswordResetEmail = email => userDb.sendPasswordResetEmail(email);
 
   const _handleUserLogout = function() {
     // module hooks
@@ -175,7 +116,7 @@ function UserProvider({ children }) {
     }
   };
 
-  const _saveCurrentUserLocally = function(userDataByPrivacy) {
+  const saveCurrentUserLocally = userDataByPrivacy => {
     const { id, publicInfo, privateInfo, serverInfo } = userDataByPrivacy;
     if (publicInfo) {
       _savePublicUserInfo(id, publicInfo);
@@ -185,13 +126,47 @@ function UserProvider({ children }) {
     if (serverInfo) setServerInfo(serverInfo);
   };
 
+  useEffect(() => {
+    // did mount
+    userDb.listenToCurrentUser((err, userData) => {
+      console.log("listening to curr user:", userData);
+      if (err) {
+        return console.log(err);
+      } else if (!userData && userId) {
+        //user logged out
+        _handleUserLogout();
+        setUserId(null);
+      } else if (userData) {
+        //new data
+        saveCurrentUserLocally(userData);
+        if (!user && userData.publicInfo) {
+          _handleUserEstablished({
+            id: userData.publicInfo.id,
+            publicInfo: userData.publicInfo
+          });
+        }
+      }
+    });
+  }, []);
+
+  // _privateMembers not passed as props
   const _listenToPublicUserData = function(userId) {
     userDb.listenToUser(userId, (err, user) => {
       _savePublicUserInfo(userId, user);
     });
   };
 
-  const _saveUserToState = user => {
+  const _updateUser = function() {
+    const user = this;
+    const uid = user.id;
+    delete user.save;
+    delete user.id;
+    delete user.displayName;
+    userDb.saveToUsersCollection(uid, { publicInfo: user });
+    _savePublicUserInfo(uid, user); // reapply deleted properties
+  };
+
+  const _saveUser = user => {
     users[user.id] = user;
     setUsers(users);
   };
@@ -201,242 +176,41 @@ function UserProvider({ children }) {
     user.displayName = user.email;
     user.save = _updateUser;
     user.id = userId;
-    _saveUserToState(user);
+    _saveUser(user);
     return user;
   };
 
   const props = {
+    user,
     userId,
     users,
-    user,
     fullUser,
-    createUserWithEmail,
     getUserById,
+    createUserWithEmail,
     login,
     logout,
     searchByField,
     sendPasswordResetEmail
   };
 
-  useEffect(() => {
-    // did mount
-    _listenToCurrentUser();
-  }, []);
-
   return <UserContext.Provider value={props}>{children}</UserContext.Provider>;
 }
 
 export { UserContext, UserProvider };
 
-// class UserStore {
-//   userId = null;
-//   usersMap = new Map(); // map of publicInfo for each user
-//   privateInfo = null; //only reads/writes by user
-//   serverInfo = null; //only user reads, only server writes
-
-//   init() {
-//     _listenToCurrentUser();
-//   }
-
-//   /**
-//    * callback executed when the user logs in, args: user
-//    * @param {function} callback
-//    */
-//   onLogin(callback) {
-//     _onLoginTriggers.push(callback);
-//   }
-
-//   /**
-//    * callback executed when the user logs out, args: user
-//    * @param {function} callback
-//    */
-//   onLogout(callback) {
-//     _onLogoutTriggers.push(callback);
-//   }
-
-//   get user() {
-//     return this.usersMap.get(this.userId);
-//   }
-
-//   get fullUser() {
-//     return {
-//       id: this.userId,
-//       public: this.user,
-//       private: this.privateInfo,
-//       server: this.serverInfo
-//     };
-//   }
-
-//   /**
-//    * returns the public user info from a given user id
-//    * & applies a listener to their data
-//    * @param {string} userId
-//    */
-//   getUserById(userId) {
-//     const user = this.usersMap.get(userId);
-//     if (!user) {
-//       _listenToPublicUserData(userId);
-//     }
-//     return user;
-//   }
-
-//   /**
-//    * logs in a user with an email and password
-//    * callback called w/ args: err, res
-//    * @param {Object} user
-//    * @param {string} user.email
-//    * @param {string} user.password
-//    * @param {function} callback
-//    */
-//   login(user, callback) {
-//     userDb.login(user, callback);
-//   }
-
-//   /**
-//    * logs out the current user
-//    */
-//   logout() {
-//     _handleUserLogout();
-//     userDb.logout(this.user);
-//     this.userId = null;
-//   }
-
-//   /**
-//    * creates a user with an email and password
-//    * callback called w/args: err, res
-//    * @param {Object} user
-//    * @param {string} user.email
-//    * @param {string} user.password
-//    * @param {function} callback
-//    */
-//   createUserWithEmail(user, callback) {
-//     if (!user || !user.email || !user.password) {
-//       return callback({
-//         message: "You must provide an email and password"
-//       });
-//     }
-
-//     userDb.createUserWithEmail(user, (err, userDataByPrivacy) => {
-//       if (err) return callback(err);
-//       _saveCurrentUserLocally(userDataByPrivacy);
-//       callback(err, userDataByPrivacy);
-//     });
-//   }
+const _onLoginTriggers = [];
+const _onLogoutTriggers = [];
 
 // /**
-//  * find a user by a publicInfo field
-//  * @param {string} query
-//  * @param {string} field
+//  * callback executed when the user logs in, args: user
+//  * @param {function} callback
 //  */
-// async searchByField = (query, field) => {
-//   let results = await userDb.searchByField(query, field);
-//   debugger;
-//   results = results || {};
-//   let formattedResults = [];
-//   Object.keys(results).forEach(uid => {
-//     if (uid !== this.userId) {
-//       this.getUserById(uid); // apply a listener
-//       const formattedUser = _savePublicUserInfo(uid, results[uid]);
-//       formattedResults.push(formattedUser);
-//     }
-//   });
-//   return formattedResults;
-// }
+// const onLogin = callback => _onLoginTriggers.push(callback);
 
-//   /**
-//    * sends a reset email to an email address if the account exists
-//    * @returns {Promise} password reset result
-//    */
-//   sendPasswordResetEmail(email) {
-//     return userDb.sendPasswordResetEmail(email);
-//   }
-// }
-
-// // Private members. Not accessible from views.
-
-// let _onLoginTriggers = [];
-// let _onLogoutTriggers = [];
-
-// const _listenToCurrentUser = function() {
-//   userDb.listenToCurrentUser((err, userData) => {
-//     if (err) {
-//       return console.log(err);
-//     } else if (!userData && userStore.userId) {
-//       //user logged out
-//       _handleUserLogout();
-//       userStore.userId = null;
-//     } else {
-//       //new data
-//       let shouldExecEstablished = !userStore.user && userData.publicInfo;
-//       _saveCurrentUserLocally(userData);
-//       if (shouldExecEstablished) {
-//         _handleUserEstablished({
-//           id: userData.publicInfo.id,
-//           publicInfo: userData.publicInfo
-//         });
-//       }
-//     }
-//   });
-// };
-
-// const _updateUser = function() {
-//   const user = this;
-//   const uid = user.id;
-//   delete user.save;
-//   delete user.id;
-//   delete user.displayName;
-//   userDb.saveToUsersCollection(uid, { publicInfo: user });
-//   _savePublicUserInfo(uid, user); // reapply deleted properties
-// };
-
-// const _handleUserLogout = function() {
-//   const user = userStore.fullUser;
-//   // module hooks
-//   try {
-//     _onLogoutTriggers.forEach(event => {
-//       event(user);
-//     });
-//   } catch (err) {
-//     debugger;
-//   }
-// };
-
-// const _handleUserEstablished = function(user) {
-//   //module hooks
-//   try {
-//     _onLoginTriggers.forEach(event => event(user));
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
-// const _saveCurrentUserLocally = function(userDataByPrivacy) {
-//   const { id, publicInfo, privateInfo, serverInfo } = userDataByPrivacy;
-//   if (publicInfo) {
-//     _savePublicUserInfo(id, publicInfo);
-//     if (publicInfo && publicInfo.isOnline) {
-//       userStore.userId = id;
-//     }
-//   }
-//   if (privateInfo) {
-//     userStore.privateInfo = privateInfo;
-//   }
-//   if (serverInfo) {
-//     userStore.serverInfo = serverInfo;
-//   }
-// };
-
-// const _listenToPublicUserData = function(userId) {
-//   userDb.listenToUser(userId, (err, user) => {
-//     _savePublicUserInfo(userId, user);
-//   });
-// };
-
-// const _savePublicUserInfo = function(userId, user) {
-//   if (!user) return;
-//   user.displayName = user.email;
-//   user.save = _updateUser;
-//   user.id = userId;
-//   userStore.usersMap.set(userId, user);
-//   return user;
+// /**
+//  * callback executed when the user logs out, args: user
+//  * @param {function} callback
+//  */
+// const onLogout = callback => {
+//   _onLogoutTriggers.push(callback);
 // };
